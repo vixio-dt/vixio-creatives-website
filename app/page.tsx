09ae9d907e2172725a80514d822e3744080068ea
Denis Tam <dt@vixiocreatives.com>
2026-06-09 18:31:20 +0000

    feat: home page, repositioned to creative label canon

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
 import { ScrollLogoReveal } from '@/components/home/ScrollLogoReveal'
+import { LabelStatement } from '@/components/home/LabelStatement'
+import { WhatWeMake } from '@/components/home/WhatWeMake'
+import { NowInProduction } from '@/components/home/NowInProduction'
+import { TheBar } from '@/components/home/TheBar'
+import { HomeCTA } from '@/components/home/HomeCTA'
 
 export default function Home() {
   return (
     <>
       <ScrollLogoReveal />
+      <LabelStatement />
+      <WhatWeMake />
+      <NowInProduction />
+      <TheBar />
+      <HomeCTA />
     </>
   )
 }
